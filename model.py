3f2c52e4c4941cb435be01c74ce34bd005ded81c
Pranav Addepalli <pranav.addepalli@gmail.com>
2019-05-04 19:19:56 -0400

    Update model.py

--- model.py
+++ model.py
@@ -5,88 +5,115 @@ Created on Thu May  2 12:37:03 2019
 
 @author: pranavaddepalli
 """
+#%% Imports
 import numpy as np
 import tensorflow as tf
-
 from tensorflow.keras import layers
 from sklearn.model_selection import train_test_split
 
+
 np.set_printoptions(precision=3, suppress=True)
 
+#%% Preprocessing
+def preprocess(raw_data, infill):
+    temperatures = []
+    
+    for a in raw_data:
+        for i in a:
+            temperatures.append(i)
+    temperatures = np.asarray(temperatures)
+    
+    print("Total number of data points / temperature values is ",len(temperatures))
+    
+    data = np.empty(shape=(len(raw_data)*len(raw_data[0]),4))
+    #col 0 is X
+    #col 1 is Y
+    #col 2 is time
+    #each row is for every measurement that happened
+    
+    r = 0
+    for col in range(0, np.size(raw_data, 1)):
+        for row in range(0, np.size(raw_data, 0)):
+            if col == 0:
+                 x = 70
+                 y = -70
+            elif col == 1:
+                x = 55
+                y = 56
+            elif col == 2:
+                x = 32.4
+                y = -41.8
+            elif col ==3:
+                x = 24
+                y = 15
+            elif col == 4:
+                x = 0
+                y = -12
+            elif col == 5:
+                x = -22.5
+                y = 33
+            elif col == 6:
+                x = -45
+                y = 48
+            else:
+                x = -72.5
+                y = 56
+            
+            data[r][0] = x
+            data[r][1] = y
+            data[r][2] = r
+            data[r][3] = infill
+            r += 1
+    print("Finished preprocessing")
+    return(data, temperatures)
 
 
-#fixing the dataset
-raw_data = np.genfromtxt('Pranav_10percentLines.csv', delimiter=',')
-print("Raw data shape is ", raw_data.shape)
-
-temperatures = []
-
-for a in raw_data:
-    for i in a:
-        temperatures.append(i)
-temperatures = np.asarray(temperatures)
-
-temperatures[0] = 49.58
-print("Total number of data points / temperature values is ",len(temperatures))
-
-data = np.empty(shape=(282255,3))
-#col 0 is X
-#col 1 is Y
-#col 2 is time
-#each row is for every measurement that happened
-
-r = 0
-for col in range(0, np.size(raw_data, 1)):
-    for row in range(0, np.size(raw_data, 0)):
-        if col == 0:
-             x = 70
-             y = -70
-        elif col == 1:
-            x = 55
-            y = 56
-        elif col == 2:
-            x = 32.4
-            y = -41.8
-        elif col ==3:
-            x = 24
-            y = 15
-        elif col == 4:
-            x = 0
-            y = -12
-        elif col == 5:
-            x = -22.5
-            y = 33
-        elif col == 6:
-            x = -45
-            y = 48
-        else:
-            x = -72.5
-            y = 56
-        
-        data[r][0] = x
-        data[r][1] = y
-        data[r][2] = r
-        r += 1
+#%% Creating dataset and splitting
+ten_percent = np.delete(np.genfromtxt('Pranav_10percentLines.txt', delimiter=','), [2, 3, 6], 1)
+twenty_percent = np.genfromtxt('20pLines.csv', delimiter=',')[:,:-1]
 
 
-print("Finished pre-processing")
+'''
+normalized:
+    10 is 0
+    20 is 0.25
+    30 is 0.5
+    40 is 0.75
+    50 is 1
+'''
 
+ten_data,ten_temps = preprocess(ten_percent, 0)
 
+twenty_data, twenty_temps = preprocess(twenty_percent, 0.2)
+
+data = np.concatenate((ten_data, twenty_data))
+temperatures = np.concatenate((ten_temps, twenty_temps))
 x_train, x_test, y_train, y_test = train_test_split(data, temperatures, test_size=0.2, random_state=42)
 
 print("Split into training and testing - 0.2 test size")
 
+
+
+
+
+
+
+#%% Model Architecture
+cp_callback = tf.keras.callbacks.ModelCheckpoint("cp.ckpt", 
+                                                 verbose=1,
+                                                 period=10)
+
 model = tf.keras.Sequential()
 #input
-model.add(layers.Dense(4, input_dim=3, activation='relu'))
+model.add(layers.Dense(64, input_dim=4, activation='relu'))
 #hidden layer 1
-model.add(layers.Dense(12, activation='relu'))
+model.add(layers.Dense(64, activation='relu'))
 #hidden layer 2
-model.add(layers.Dense(12, activation='relu'))
+model.add(layers.Dense(64, activation='relu'))
 #output layer
 model.add(layers.Dense(1, activation='relu'))
 
-model.compile(optimizer=tf.train.AdamOptimizer(0.00001), #gradient descent
+model.compile(optimizer=tf.keras.optimizers.Adam(lr=0.00001), #adam optimizer
               loss='mse', #mean squared error
               metrics=['mae']) #mean absolute error
 
@@ -94,5 +121,13 @@ print("Model built!")
 print(model.summary())
 
 
-model.fit(x=x_train, y=y_train, batch_size=32, epochs=5, validation_data=(x_test, y_test))
+#%% Training
+model.fit(x=x_train,
+          y=y_train, 
+          batch_size=32, 
+          epochs=72, 
+          validation_data= (x_test, y_test),
+          callbacks = [cp_callback])
 
+#%% Save
+model.save('model.h5')
